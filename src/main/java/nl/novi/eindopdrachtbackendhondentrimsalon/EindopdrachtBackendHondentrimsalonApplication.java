9ac9deda6ba847f045f3f76be336442aa8918817
estherpunter <estherpunter5@hotmail.com>
2024-04-19 15:24:03 +0200

    Mappenstructuur klaargezet

---
+++ src/main/java/nl/novi/eindopdrachtbackendhondentrimsalon/EindopdrachtBackendHondentrimsalonApplication.java
@@ -0,0 +1,13 @@
+package nl.novi.eindopdrachtbackendhondentrimsalon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EindopdrachtBackendHondentrimsalonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EindopdrachtBackendHondentrimsalonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/novi/eindopdrachtbackendhondentrimsalon/EindopdrachtBackendHondentrimsalonApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.eindopdrachtbackendhondentrimsalon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EindopdrachtBackendHondentrimsalonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
