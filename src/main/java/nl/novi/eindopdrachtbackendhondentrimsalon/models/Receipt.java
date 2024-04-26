650c91eee03587c617ad2a9559a1f35da350f922
estherpunter <estherpunter5@hotmail.com>
2024-04-26 12:46:06 +0200

    Receipt class gevuld

--- src/main/java/nl/novi/eindopdrachtbackendhondentrimsalon/models/Receipt.java
+++ src/main/java/nl/novi/eindopdrachtbackendhondentrimsalon/models/Receipt.java
@@ -1,4 +1,50 @@
 package nl.novi.eindopdrachtbackendhondentrimsalon.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+import java.util.List;
+
+@Entity
 public class Receipt {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @OneToMany
+    private List<Treatment> treatments;
+
+    @OneToMany
+    private List<Product> products;
+
+    public Receipt() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Treatment> getTreatments() {
+        return treatments;
+    }
+
+    public void setTreatments(List<Treatment> treatments) {
+        this.treatments = treatments;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
 }
