740807a92cca6581d30e9ea2efd2b06170d01be3
estherpunter <estherpunter5@hotmail.com>
2024-04-26 12:44:30 +0200

    Appointment, Treatment en Product classes gevuld

--- src/main/java/nl/novi/eindopdrachtbackendhondentrimsalon/models/Appointment.java
+++ src/main/java/nl/novi/eindopdrachtbackendhondentrimsalon/models/Appointment.java
@@ -1,12 +1,84 @@
 package nl.novi.eindopdrachtbackendhondentrimsalon.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import org.apache.catalina.util.CustomObjectInputStream;
 
 import java.util.Date;
+import java.util.List;
 
+@Entity
 public class Appointment {
 
+    @Id
+    @GeneratedValue
+    private Long id;
+    private Date date;
+    @ManyToOne
+    private Customer customer;
+    @ManyToOne
+    private Dog dog;
+
+    @OneToMany
+    private List<Product> products;
+
+    @OneToMany
+    private List<Treatment> treatments;
+
+    public Appointment() {
+
+    }
+
+    public Appointment(Date date, Customer customer, Dog dog) {
+        this.date = date;
+        this.customer = customer;
+        this.dog = dog;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Dog getDog() {
+        return dog;
+    }
+
+    public void setDog(Dog dog) {
+        this.dog = dog;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    public List<Treatment> getTreatments() {
+        return treatments;
+    }
+
+    public void setTreatments(List<Treatment> treatments) {
+        this.treatments = treatments;
+    }
 }
--- src/main/java/nl/novi/eindopdrachtbackendhondentrimsalon/models/Treatment.java
+++ src/main/java/nl/novi/eindopdrachtbackendhondentrimsalon/models/Treatment.java
@@ -1,4 +1,51 @@
 package nl.novi.eindopdrachtbackendhondentrimsalon.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "treatments")
 public class Treatment {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+    private double price;
+
+    public Treatment() {
+
+    }
+
+    public Treatment(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
