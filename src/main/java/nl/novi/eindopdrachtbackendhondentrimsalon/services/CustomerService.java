ac881f9767c926e11db19717ff603cbdd854dcc8
estherpunter <estherpunter5@hotmail.com>
2024-04-26 15:02:10 +0200

    CustomerController en CustomerService toegevoegd

---
+++ src/main/java/nl/novi/eindopdrachtbackendhondentrimsalon/services/CustomerService.java
@@ -0,0 +1,15 @@
+package nl.novi.eindopdrachtbackendhondentrimsalon.services;
+
+import nl.novi.eindopdrachtbackendhondentrimsalon.models.Customer;
+
+public class CustomerService {
+    public Customer createCustomer(Customer customer) {
+    }
+
+
+    public Customer updateCustomer(Long customerId, Customer customer) {
+    }
+
+    public void deleteCustomer(Long customerId) {
+    }
+}
