003724e8c727f2831068e366a1a6932eb7cb53d5
estherpunter <estherpunter5@hotmail.com>
2024-04-27 12:27:47 +0200

    Application properites aangepast voor gebruik database

--- src/main/java/nl/novi/eindopdrachtbackendhondentrimsalon/repository/TreatmentRepository.java
+++ src/main/java/nl/novi/eindopdrachtbackendhondentrimsalon/repository/TreatmentRepository.java
@@ -1,7 +1,9 @@
 package nl.novi.eindopdrachtbackendhondentrimsalon.repository;
 
+import nl.novi.eindopdrachtbackendhondentrimsalon.models.Treatment;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class TreatmentRepository {
+public interface TreatmentRepository extends JpaRepository<Treatment, Long> {
 }
