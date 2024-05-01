05e548c50111e48475147f3a7135c5179ec4d1c2
estherpunter <estherpunter5@hotmail.com>
2024-05-01 11:25:26 +0200

    Basisinrichting voor users en rollen ingesteld

--- src/main/java/nl/novi/eindopdrachtbackendhondentrimsalon/models/User.java
+++ src/main/java/nl/novi/eindopdrachtbackendhondentrimsalon/models/User.java
@@ -1,4 +1,53 @@
 package nl.novi.eindopdrachtbackendhondentrimsalon.models;
 
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, length = 255)
+    private String password;
+
+    @OneToMany(
+            targetEntity = Role.class,
+            mappedBy = "username",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true,
+            fetch = FetchType.EAGER)
+    private Set<Role> roles = new HashSet<>();
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void addRole(Role role) {
+        this.roles.add(role);
+    }
+    public void removeRole(Role role) {
+        this.roles.remove(role);
+    }
 }
