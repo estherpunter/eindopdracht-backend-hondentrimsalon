7eb85b65831d5a8548cb27f6f67711a61076eca7
estherpunter <estherpunter5@hotmail.com>
2024-05-02 10:37:55 +0200

    Authenticatie aangepast

--- src/main/java/nl/novi/eindopdrachtbackendhondentrimsalon/controllers/AuthenticationController.java
+++ src/main/java/nl/novi/eindopdrachtbackendhondentrimsalon/controllers/AuthenticationController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 
-@CrossOrigin
 @RestController
 public class AuthenticationController {
 
--- src/main/java/nl/novi/eindopdrachtbackendhondentrimsalon/controllers/UserController.java
+++ src/main/java/nl/novi/eindopdrachtbackendhondentrimsalon/controllers/UserController.java
@@ -12,7 +12,6 @@ import java.net.URI;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
