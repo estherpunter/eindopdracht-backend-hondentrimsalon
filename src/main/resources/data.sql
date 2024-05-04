c86b27d7bfdef66aa2be6b8efa4eea3b8f44d097
estherpunter <estherpunter5@hotmail.com>
2024-05-04 16:23:03 +0200

    Users toegevoegd en security aangepast

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -24,13 +24,22 @@ VALUES ('Dog Shampoo', 12.99, 50),
        ('Brush', 8.50, 30);
 
 
--- Create Users table
-DROP TABLE IF EXISTS Users;
-CREATE TABLE Users
-(
-    id       SERIAL PRIMARY KEY,
-    username VARCHAR(50)  NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    enabled  BOOLEAN DEFAULT true
-);
+INSERT INTO users (username, password, enabled)
+VALUES ('admin', '$2a$10$H0xLFvvBYM.NtMbTlsYe4emMWIo/2DGrohgTG8PmwIQoThv3S9zjW', true);
+
+INSERT INTO roles (username, role)
+VALUES ('admin', 'ADMIN');
+
+INSERT INTO users (username, password, enabled)
+VALUES ('cashier', '$2a$10$5SP253Ev3YlFL/h/TdSP5eAgPxiEx8VyMSbrWhvxMggxzzYoZKGy.', true);
+
+INSERT INTO roles (username, role)
+VALUES ('cashier', 'CASHIER');
+
+INSERT INTO users (username, password, enabled)
+VALUES ('doggroomer', '$2a$10$Wtpz7aVnAJdbvgppY4YaQ.GlL5yV697ZkdTq29VnMp7CP0ZKl5pM2', true);
+
+INSERT INTO roles (username, role)
+VALUES ('doggroomer', 'DOGGROOMER');
+
 
