de8f518ed523fe722673e13bb8954091842b9294
estherpunter <estherpunter5@hotmail.com>
2024-05-04 13:39:05 +0200

    RequestMatchers aangezet en ingevuld

--- src/main/java/nl/novi/eindopdrachtbackendhondentrimsalon/controllers/AppointmentController.java
+++ src/main/java/nl/novi/eindopdrachtbackendhondentrimsalon/controllers/AppointmentController.java
@@ -23,7 +23,7 @@ public class AppointmentController {
     }
 
 
-    @GetMapping("/allAppointments")
+    @GetMapping("/allappointments")
     public ResponseEntity<List<AppointmentDto>> getAllAppointments() {
         List<AppointmentDto> appointmentDtos = appointmentService.getAllAppointments();
         return ResponseEntity.ok(appointmentDtos);
